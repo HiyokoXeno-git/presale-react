c335caf7dc8596d5aaf9799db836d4240c38077a
hanscakrawangsa15 <hans.cakrawangsa@gmail.com>
2026-03-30 15:13:39 +0700

    feat:wallet connect and improvement frontend 30/03/2026

--- commit_and_push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git add .
-git commit -m "feat:wallet connect and improvement frontend 30/03/2026"
-git branch -M main
-git remote add origin https://github.com/HiyokoXeno-git/presale-react.git 2>NUL
-git remote set-url origin https://github.com/HiyokoXeno-git/presale-react.git
-git push -u origin main
